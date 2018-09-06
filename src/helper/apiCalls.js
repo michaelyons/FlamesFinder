56cf53cd9ba035811a7a08a1ca163e3c93e6e216
Michael J. Lyons <mikelyons7@me.com>
2018-09-06 15:37:29 -0600

    Fix currentweather fetch :bug:

--- src/helper/apiCalls.js
+++ src/helper/apiCalls.js
@@ -1,7 +1,7 @@
 import { key } from '../key';
 
 export const getCurrentWeatherData = async () => {
-  const url = `api.openweathermap.org/data/2.5/weather?q=denver,us&units=imperial&APPID=${key}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=denver,us&units=imperial&APPID=${key}`;
   const response = await fetch(url);
   const currentCityWeather = await response.json();
   return currentCityWeather;
