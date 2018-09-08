7bfa868555cc3ba044d79333c21f0d16144e9725
Michael J. Lyons <mikelyons7@me.com>
2018-09-08 13:11:08 -0600

    Add tenHourWeather to redux store

--- src/helper/apiCalls.js
+++ src/helper/apiCalls.js
@@ -1,5 +1,9 @@
-import { key } from '../variables';
-import { currentWeatherCleaner } from './cleaners';
+import { key, keyW } from '../variables';
+import {
+  currentWeatherCleaner,
+  tenHourWeatherCleaner,
+  fiveDayWeatherCleaner
+} from './cleaners';
 
 export const getCurrentWeatherData = async () => {
   const url = `https://api.openweathermap.org/data/2.5/weather?zip=80228,us&units=imperial&APPID=${key}`;
@@ -8,9 +12,16 @@ export const getCurrentWeatherData = async () => {
   return currentWeatherCleaner(currentCityWeather);
 };
 
+export const getTenHourWeatherData = async () => {
+  const url = `http://api.wunderground.com/api/${keyW}/geolookup/conditions/hourly/forecast10day/q/denver,co.json`;
+  const response = await fetch(url);
+  const tenHourWeather = await response.json();
+  return tenHourWeatherCleaner(tenHourWeather);
+};
+
 export const getFiveDayWeatherData = async () => {
   const url = `https://api.openweathermap.org/data/2.5/forecast?zip=93103,us&units=imperial&APPID=${key}`;
   const response = await fetch(url);
   const fiveDayWeather = await response.json();
-  return fiveDayWeather;
+  return fiveDayWeatherCleaner(fiveDayWeather);
 };
