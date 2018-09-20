3c47c1565e7d71d1f7064daa7115e409bfcd8051
Michael J. Lyons <mikelyons7@me.com>
2018-09-20 13:24:16 -0600

    Add additional Firegif

---
+++ src/components/LoadingFire/LoadingFire3.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import fire from '../LoadingFire/fire.gif';
+
+const fireLoad3 = () => {
+  return (
+    <div>
+      <img src={fire} alt="fire loading gif" className="fire-gif3" />
+    </div>
+  );
+};
+
+export default fireLoad3;
